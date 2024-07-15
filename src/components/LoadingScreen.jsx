121ee634110f660512e55ee4b3017b11ebfebefb
solersol <ttpage1tk@gmail.com>
2024-07-16 02:12:12 +0300

    Vdf

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,15 +1,4 @@
-// import React from 'react';
-// import {loadingImage} from '../assets/images'; // Update with your loading image path
 
-// const LoadingScreen = () => {
-//   return (
-//     <div className="fixed inset-0 flex items-center justify-center bg-[#a7a7c5] z-50">
-//       <img src={loadingImage} alt="Loading" className="w-16 h-16 animate-spin" />
-//     </div>
-//   );
-// };
-
-// export default LoadingScreen;
 
 
 import React, { useEffect, useRef } from 'react';
