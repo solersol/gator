ddf6dacb3e0172d26ba131f9972bd819d4dfa71e
solersol <ttpage1tk@gmail.com>
2024-07-16 23:23:05 +0300

    vfd

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,8 @@ const Hero = () => {
     <section id="home" className="w-full flex flex-col justify-center items-center min-h-screen  pb-56 bg-cover bg-center">
       <h1 className="flex justify-center font-anton text-[70px] mb-0 text-[#dfe2ff]">$GATOR</h1>
       <h2 className="flex justify-center font-anton text-[22px] mb-10 mt-0 text-[#dfe2ff]">INVESTI-GATOR RUG DETECTIVE</h2>
-      <h1 className="flex justify-center font-anton text-[35px] mb-0 text-center px-4 break-words text-[#dfe2ff]">CA: INCOMING </h1>
-      <a href="https://pump.fun/"><button className="mt-6 bg-[#5e5e78] text-[#dfe2ff] rounded-full px-10 py-4 hover:bg-[#5db226] font-anton text-[24px]">
+      <h1 className="flex justify-center font-anton text-[35px] mb-0 text-center px-4 break-words text-[#dfe2ff]">CA: 5bDLAe8rQ3nKakn8KNiubntVeXEqhLSqVNZxVAiEpump </h1>
+      <a href="https://pump.fun/5bDLAe8rQ3nKakn8KNiubntVeXEqhLSqVNZxVAiEpump"><button className="mt-6 bg-[#5e5e78] text-[#dfe2ff] rounded-full px-10 py-4 hover:bg-[#5db226] font-anton text-[24px]">
         BUY $GATOR
       </button></a>
     </section>
--- src/sections/Tokenomics.jsx
+++ src/sections/Tokenomics.jsx
@@ -34,7 +34,7 @@ const Tokenomics = () => {
 
       <div className="flex flex-col md:grid md:grid-cols-2 gap-4  ">
         <button className="bg-[#5e5e78] text-white rounded-3xl hover:bg-[#5db226]  font-anton text-[20px] border-4 border-solid border-black py-[30px] px-[150px]">
-          <a href="https://pump.fun/" target="_blank" className="">PUMP.FUN</a>
+          <a href="https://pump.fun/5bDLAe8rQ3nKakn8KNiubntVeXEqhLSqVNZxVAiEpump" target="_blank" className="">PUMP.FUN</a>
         </button>
         <button className="bg-[#5e5e78] text-white rounded-3xl hover:bg-[#5db226]  font-anton text-[20px] border-4 border-solid border-black py-[30px] px-[150px]">
           <a href="https://x.com/GatorOnSol_" target="_blank" className="">TWITTER</a>
